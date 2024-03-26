fa9a4976afddcb3d6cf0278127b07a737de9f58b
stephen owino <Sowino939@gmail.com>
2024-03-26 15:49:30 +0300

    create an arrow file

---
+++ index.js
@@ -0,0 +1,21 @@
+let a=2000
+let b=100
+
+const divide = (a, b) => {
+    console.log(`divide ${a}`);
+    console.log(`divide ${b}`);
+    return a /b;
+    
+  }
+   
+
+
+
+let x=2
+const square = x => 2** 2 ;
+console.log(square);
+
+
+const add = (x) => c + d;
+var c = 3
+var d = 4
